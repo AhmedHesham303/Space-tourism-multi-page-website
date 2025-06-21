67b7b812fd67b8e6834b5c808a2697ad58b5f6c5
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-21 03:39:23 +0300

    goining to destenation page when pressing explore btn in home page

--- JS_Files/home.js
+++ JS_Files/home.js
@@ -2,6 +2,10 @@ const exploreBtn = document.querySelector(".display-button");
 const navSteps = document.querySelectorAll(".nav-step");
 const navHome = document.querySelector(".home-page-link");
 
+exploreBtn.addEventListener("click", () => {
+  window.location.href = "../destination-moon.html";
+});
+
 function changeSelectedLinkState() {
   navSteps.forEach((navStep) => {
     navStep.classList.remove("selected-nav-link");
