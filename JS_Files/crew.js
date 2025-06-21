6b984833d969a1cffc0bf2dc30633230b4799ea3
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-21 19:32:52 +0300

    making crew page dinamic by fetching data from data.json file

--- JS_Files/crew.js
+++ JS_Files/crew.js
@@ -8,3 +8,45 @@ function changeSelectedLinkState() {
 }
 
 changeSelectedLinkState();
+
+async function loadData(crewMemberNum) {
+  try {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    displayDestination(data.crew[crewMemberNum]);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function displayDestination(crewMember) {
+  document.querySelector(".crew-member-name").textContent = crewMember.name;
+  document.querySelector(".crew-member-title").textContent = crewMember.role;
+
+  document.querySelector(".crew-member-img").src = crewMember.images.png;
+
+  document.querySelector(".crew-member-description").textContent =
+    crewMember.description;
+}
+
+// function handelCrewMemberChoice() {
+//   let choiceNum = 0;
+//   destenationChoices.forEach((choice) => {
+//     choice.addEventListener("click", () => {
+//       if (choice.textContent === "Moon") choiceNum = 0;
+//       else if (choice.textContent === "Mars") choiceNum = 1;
+//       else if (choice.textContent === "Europa") {
+//         choiceNum = 2;
+//       } else if (choice.textContent === "Titan") {
+//         choiceNum = 3;
+//       }
+//       loadData(choiceNum);
+//     });
+//   });
+// }
+
+// handelDestenationChoice();
+
+loadData(4);
+
+changeSelectedLinkState();
