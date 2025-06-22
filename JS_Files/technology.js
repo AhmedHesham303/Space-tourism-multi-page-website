6fb280cfd36ded68eaefe26b3cb6dba5a2f9dbbb
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-22 04:28:24 +0300

    fixing main nav bugs

--- JS_Files/technology.js
+++ JS_Files/technology.js
@@ -1,37 +1,37 @@
-const technologySteps = document.querySelectorAll(".nav-step");
-const navTechnology = document.querySelector(".technology-page-link");
-
+const technologyNavSteps = document.querySelectorAll(".nav-step");
+const technologyPageLink = document.querySelector(".technology-page-link");
+const technologySteps = document.querySelectorAll(".technology-step");
 function changeSelectedLinkState() {
-  technologySteps.forEach((navStep) => {
+  technologyNavSteps.forEach((navStep) => {
     navStep.classList.remove("selected-nav-link");
   });
-  navTechnology.classList.add("selected-nav-link");
+  technologyPageLink.classList.add("selected-nav-link");
 }
 
 function handelTechChoice() {
-  navSteps.forEach((choice, index) => {
+  technologySteps.forEach((choice, index) => {
     choice.addEventListener("click", () => {
       loadData(index);
     });
   });
 }
 
-// async function loadData(technologyNum) {
-//   try {
-//     const response = await fetch("./data.json");
-//     const data = await response.json();
-//     displayCrewMemberInfo(data.technology[technologyNum]);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
+async function loadData(technologyNum) {
+  try {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    displayCrewMemberInfo(data.technology[technologyNum]);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
-// function displayCrewMemberInfo(technology) {
-//   document.querySelector(".technology-name").textContent = technology.name;
-//   document.querySelector(".technology-img").src = technology.images.portrait;
-//   document.querySelector(".technology-description").textContent =
-//     technology.description;
-// }
+function displayCrewMemberInfo(technology) {
+  document.querySelector(".technology-name").textContent = technology.name;
+  document.querySelector(".technology-img").src = technology.images.portrait;
+  document.querySelector(".technology-description").textContent =
+    technology.description;
+}
 
-// handelTechChoice();
+handelTechChoice();
 changeSelectedLinkState();
