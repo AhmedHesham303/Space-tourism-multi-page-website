9be4dc1787df292b3fd2d89ef011d0f8a4036602
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-30 00:15:52 +0300

    fixing basePath error

--- JS_Files/home.js
+++ JS_Files/home.js
@@ -1,21 +1,21 @@
-const exploreBtn = document.querySelector(".display-button");
-const homeNavSteps = document.querySelectorAll(".nav-step");
-const navHome = document.querySelector(".home-page-link");
-const isLocal =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1" ||
-  window.location.protocol === "file:";
-const basePath = isLocal ? "" : "/Space-tourism-multi-page-website";
+document.addEventListener("DOMContentLoaded", () => {
+  const exploreBtn = document.querySelector(".display-button");
+  const homeNavSteps = document.querySelectorAll(".nav-step");
+  const navHome = document.querySelector(".home-page-link");
 
-exploreBtn.addEventListener("click", () => {
-  window.location.href = `${basePath}/destination.html`;
-});
-
-function changeSelectedLinkState() {
-  homeNavSteps.forEach((navStep) => {
-    navStep.classList.remove("selected-nav-link");
+  exploreBtn.addEventListener("click", () => {
+    console.log(basePath);
+    window.location.href = `${basePath}/destination.html`;
   });
-  navHome.classList.add("selected-nav-link");
-}
 
-changeSelectedLinkState();
+  function changeSelectedLinkState() {
+    homeNavSteps.forEach((navStep) => {
+      navStep.classList.remove("selected-nav-link");
+    });
+    if (navHome) {
+      navHome.classList.add("selected-nav-link");
+    }
+  }
+
+  changeSelectedLinkState();
+});
